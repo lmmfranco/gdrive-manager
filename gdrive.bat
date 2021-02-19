4e72a1f826735c99cb5dc25773a7df232e632d29
Luiz Marcelo Martins Franco <luiz.franco@fpf.br>
2021-02-19 18:14:04 -0400

    Make batch script check for installed python executables

--- gdrive.bat
+++ gdrive.bat
@@ -1,2 +1,18 @@
 @echo off
-py %~dp0\gdrive.py %*
+
+rem Check if python launcher is installed
+if exist %SYSTEMROOT%\py.exe (
+    py %~dp0\gdrive.py %*
+    goto end
+)
+
+rem Check if python3 alias is usable
+python3 --version > NUL 2>&1
+if %ERRORLEVEL% == 0 (
+    python3 %~dp0\gdrive.py %*
+    goto end
+)
+
+rem If everything else fails, try running default python
+python %~dp0\gdrive.py %*
+:end
